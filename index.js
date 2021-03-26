8f0e576abc231b08272caac393776a822c390623
Dragos <d.vecerdea@gmail.com>
2021-03-26 17:40:54 +0200

    run script action

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
+const { testAction } = require('./actions.js');
 const program = new Command();
 program.version('0.0.1');
 
 program
-.description('test command', {
-    username: 'user to login',
-    password: 'password for user, if required'
-  })
-  .option('-p, --path <path>', 'path to test set', '.')
-  .action((s) => {
-    console.log('Hello world');
+    .description('test command')
+    .option('-p, --path <path>', 'path to test set', '.')
+    .action(({path}) => {
+        testAction(path)
   });
 
 program.parse(process.argv);
